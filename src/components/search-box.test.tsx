ee2b696939d44e492399f788b266794fd074e36b
Mark Kenneth Pelayo <mkgpdev@gmail.com>
2026-06-23 18:20:28 +0800

    chore: remove unused waitFor import

--- src/components/search-box.test.tsx
+++ src/components/search-box.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/preact';
+import { fireEvent, render, screen } from '@testing-library/preact';
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import { SearchBox } from './search-box';
 
